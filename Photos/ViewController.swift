8bdf70a9c6f1482f3f92dfb9ada6d0766ab72d3e
Kazuya Ueoka <info@fromkk.info>
2018-11-11 19:21:16 +0900

    Initial Commit

---
+++ Photos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photos
+//
+//  Created by Kazuya Ueoka on 2018/11/11.
+//  Copyright © 2018 Timers Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
